ecd245a81aaabc28eddf08868d3139701b7bdd61
Jan Helbling <jh@jan-helbling.ch>
2023-08-10 16:21:23 +0200

    auth_db.sql -> mysql

--- sql/auth_db.sql
+++ sql/auth_db.sql
@@ -3,16 +3,16 @@ CREATE SCHEMA auth;
 CREATE SEQUENCE auth.user_id_seq;
 CREATE SEQUENCE auth.group_id_seq;
 
-CREATE TABLE auth."user" (
-  id integer NOT NULL DEFAULT nextval('auth.user_id_seq'),
+CREATE TABLE auth.user (
+  id integer NOT NULL DEFAULT NEXT VALUE FOR auth.user_id_seq,
   login text UNIQUE NOT NULL,
   password text,
 
   PRIMARY KEY (id)
 );
 
-CREATE TABLE auth."group" (
-  id integer NOT NULL DEFAULT nextval('auth.group_id_seq'),
+CREATE TABLE auth.group (
+  id integer NOT NULL DEFAULT NEXT VALUE FOR auth.group_id_seq,
   name text UNIQUE NOT NULL,
   description text,
 
@@ -23,16 +23,16 @@ CREATE TABLE auth.user_group (
   user_id integer NOT NULL,
   group_id integer NOT NULL,
 
-  FOREIGN KEY (user_id) REFERENCES auth."user" (id),
-  FOREIGN KEY (group_id) REFERENCES auth."group" (id)
+  FOREIGN KEY (user_id) REFERENCES auth.user (id),
+  FOREIGN KEY (group_id) REFERENCES auth.group (id)
 );
 
 CREATE TABLE auth.group_rights (
   group_id integer NOT NULL,
-  "right" text NOT NULL,
+  rights text NOT NULL,
   granted boolean NOT NULL,
 
-  FOREIGN KEY (group_id) REFERENCES auth."group" (id)
+  FOREIGN KEY (group_id) REFERENCES auth.group (id)
 );
 
 CREATE TABLE auth.user_config (
@@ -40,26 +40,26 @@ CREATE TABLE auth.user_config (
   cfg_key text NOT NULL,
   cfg_value text,
 
-  FOREIGN KEY (user_id) REFERENCES auth."user" (id)
+  FOREIGN KEY (user_id) REFERENCES auth.user (id)
 );
 
 CREATE TABLE auth.schema_info (
-  tag text,
+  tag varchar(512),
   login text,
   itime timestamp DEFAULT now(),
   PRIMARY KEY (tag)
 );
 
 CREATE TABLE auth.session (
-  id text,
-  ip_address inet,
+  id varchar(512),
+  ip_address binary(16),
   mtime timestamp,
 
   PRIMARY KEY (id)
 );
 
 CREATE TABLE auth.session_content (
-  session_id text,
+  session_id varchar(256) PRIMARY KEY,
   sess_key text,
   sess_value text,
 
